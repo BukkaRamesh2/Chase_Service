156d1b361fc9cec521d7319e5d7f5b3575ff4102
BukkaRamesh <bukkaramesh.b@gmail.com>
2025-05-21 16:56:02 -0500

    Added test

---
+++ src/main/java/com/chase/Test.java
@@ -0,0 +1,159 @@
+package com.chase;
+
+
+
+/*
+
+
+Spring MVC   -- Backend and Frontend
+Spring Boot -- RESTFUL API applications   -- 
+
+Standalone applicaitons
+Comandline applicaitons
+
+
+
+File  -- import --> exisiting maven applicaiotn --> 
+
+
+ JSON
+ 
+ {
+    user: abc,
+    
+ }
+ 
+ XML
+ 
+  <xml>
+  <account deials> username </
+  <value> 
+   <account deials> username </
+  <value> 
+   <account deials> username </
+  <value>  <account deials> username </
+  <value> 
+   </xml>
+ 
+
+Core
+
+  Class
+  Interface
+  Enums
+  DataTypes   -- strings , arrays
+  Varaibles
+  Access Specifiers 
+  Method / object 
+  OOPS -- inhertance, abstraction, encapsulation, polymorphism
+  Descions making statments -- if, if else, nested if, switch, break and continue
+  Loops -- for, do while, foreach, 
+  Collections   
+  Exceptions
+  MultiThreading
+  Files
+
+
+
+
+Chase service 
+
+
+
+Entity class or POJO class  
+
+9  Team 
+
+
+Customer -- Ramesh
+   id
+   firstname
+   lastname
+   address
+   email
+   mobilenumber
+   status
+   
+Employee   -- Pavan
+   empId
+   firstname
+   lastname
+   address
+   email
+   mobilenumber
+   status
+ 
+Transactions  -- Vikas
+  		 transacastionid
+   *     transactiontype
+	 *    datetime
+	 *    amount
+	 *    branchCode
+	 *    status
+  
+Accounts  -- Meshwa
+   accountid
+   accountnumber
+	 *    accounttype
+	 *    balance   -- 
+	 *    createdDate  -- date
+	 *    customerId
+	 *    status
+   
+Loans  -- Narendra
+     LoanId
+     loanType
+     ROI
+     customerId
+     loanTerm
+     loanStartDate
+     loanEndDate
+     status
+  
+Cards  --   Preethi
+ cardId
+ cardType
+ cardNumber
+ expDate
+ cvv
+ status
+ 
+Insurance  -- Anvesh
+   insuranceId
+   insuranceType
+   insuranceTerm
+   nominees
+   startDate
+   endDate
+   status
+ 
+Clients   -- Ankith 
+clientId
+clinetName
+clinetAddress
+clientStatus
+
+Branches   -- Prudhvi
+	 *   branchid
+	 *   branch name
+	 *   branch address
+	 *   branch manager
+	 *   branch code
+	 *   Timings
+
+
+
+
+
+
+
+
+
+
+
+*/
+public class Test {
+
+	
+
+}
