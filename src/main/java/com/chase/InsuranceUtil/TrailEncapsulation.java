a1b82c31ed286e9c17660579b567a23872d59f01
Anveshc3700 <anveshc3700@gmail.com>
2025-05-28 13:07:17 -0500

    Added OOPS

---
+++ src/main/java/com/chase/InsuranceUtil/TrailEncapsulation.java
@@ -0,0 +1,11 @@
+package com.chase.InsuranceUtil;
+
+import com.chase.entity.Insurance;
+
+public class TrailEncapsulation extends Insurance{
+	public static void main(String[] args) {
+		// Encapsulation 
+		
+	}
+
+}
