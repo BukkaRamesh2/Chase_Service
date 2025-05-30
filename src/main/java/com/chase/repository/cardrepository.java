cb8028ac5552bfd71bf5b3125a091212ee1eadb8
preethi-246 <nagapreethi246@gmail.com>
2025-05-29 21:14:57 -0500

    new commit

--- src/main/java/com/chase/repository/cardrepository.java
+++ src/main/java/com/chase/repository/cardrepository.java
@@ -6,4 +6,8 @@ import com.chase.entity.Card;
 
 public interface cardrepository extends JpaRepository<Card,Long>{
 
+	Object findByCardNumber(String cardNumber);
+
+	Card getBycardNumber(String cardNumber);
+
 }
--- src/main/java/com/chase/service/Cardserviceimplementation.java
+++ src/main/java/com/chase/service/Cardserviceimplementation.java
@@ -2,41 +2,72 @@ package com.chase.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.chase.entity.Card;
+import com.chase.repository.cardrepository;
 
 public class Cardserviceimplementation implements Cardservice {
-	public Cardserviceimplementation() {
-		
-	}
 
-	@Override
-	public Card addCard(Card card) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Autowired
+    cardrepository cardRepository;
 
-	@Override
-	public Card getCard(String cardNumber) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Cardserviceimplementation() {
 
-	@Override
-	public List<Card> getAllCard() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
 
-	@Override
-	public Card updateCard(Card card) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Card addCard(Card card) {
+        if (card.getCardType() == null) {
+            System.out.println("The customer doesn't have a card type");
+        } else {
+            switch (card.getCardType().toLowerCase()) {
+                case "debit":
+                    System.out.println("The customer has a debit card");
+                    break;
+                case "credit":
+                    System.out.println("The customer has a credit card");
+                    break;
+                default:
+                    System.out.println("The customer has another card type");
+            }
+        }
+        return null;
+    }
 
-	@Override
-	public Card deletecard(String cardNumber) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Card getCard(String cardNumber) {
+        if (cardNumber != null && cardNumber.length() > 0) {
+            System.out.println("Customer has a card");
+        } else {
+            System.out.println("Customer doesn't have a card");
+            return cardRepository.getBycardNumber(cardNumber);
+        }
+        return null;
+    }
 
+    @Override
+    public List<Card> getAllCard() {
+        List<Card> cards = cardRepository.findAll();
+        if (cards.isEmpty()) {
+            System.out.println("There are no cards found");
+        } else {
+            cards.forEach(card -> {
+                if (card.getStatus().equalsIgnoreCase("active")) {
+                    System.out.println("The card is in active status");
+                }
+            });
+        }
+        return null;
+    }
+
+    @Override
+    public Card updateCard(Card card) {
+        return null;
+    }
+
+    @Override
+    public Card deletecard(String cardNumber) {
+        return null;
+    }
 }
