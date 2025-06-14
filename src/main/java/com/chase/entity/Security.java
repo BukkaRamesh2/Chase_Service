9aa534c0b3678f447b4ded02f870bb6d0262ed01
konda <konda@chandana-HP>
2025-06-14 06:36:21 -0400

    added entity

--- src/main/java/com/chase/entity/Security.java
+++ src/main/java/com/chase/entity/Security.java
@@ -1,5 +1,72 @@
 package com.chase.entity;
 
+import java.time.LocalDateTime;
+
 public class Security {
 
+	private Long securityId;         
+	private Long customerId;                 // can link to Customer
+	private String username;        
+	private String password;      
+	protected boolean mfaEnabled;         
+	private LocalDateTime lastLoginDate;    
+	protected boolean accountStatus; // active or lock 
+	
+	public Long getSecurityId() {
+        return securityId;
+    }
+
+    public void setSecurityId(Long securityId) {
+        this.securityId = securityId;
+    }
+
+	public Long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isMfaEnabled() {
+		return mfaEnabled;
+	}
+
+	public void setMfaEnabled(boolean mfaEnabled) {
+		this.mfaEnabled = mfaEnabled;
+	}
+
+	public LocalDateTime getLastLoginDate() {
+		return lastLoginDate;
+	}
+
+	public void setLastLoginDate(LocalDateTime lastLoginDate) {
+		this.lastLoginDate = lastLoginDate;
+	}
+
+	public boolean isAccountStatus() {
+		return accountStatus;
+	}
+
+	public void setAccountStatus(boolean accountStatus) {
+		this.accountStatus = accountStatus;
+	}
+
 }
+
